f5d3aca8f5b1cb9af14f4062212c8a433c1209cc
Ronit Pattanayak <ronitp@0587361225.wireless.umich.net>
2022-03-13 17:15:35 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Movie Theater Seating Challenge
+
+## Problem:
+Fulfill reservations for a given movie theater.
+
+## Specifications Given :
+10 x 20 movie theater
+
+
+
+## Usage
+
+
---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WalmartSWEQ
+//
+//  Created by Ronit Pattanayak on 3/13/22.
+//
+
+#include <iostream>
+
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
